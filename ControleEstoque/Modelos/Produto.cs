27f9c2074592aa3e4d1e64665f2d4d204ac2dcbf
moraismarcio <marcioc.morais@hotmail.com>
2018-08-25 19:56:44 -0300

    Implementando propriedades da classe (Modelo) Produto

--- ControleEstoque/Modelos/Produto.cs
+++ ControleEstoque/Modelos/Produto.cs
@@ -4,13 +4,21 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Controllers
+namespace Modelos
 {
     public class Produto
     {
         public int ProdutoId { get; set; }
 
         public string Nome { get; set; }
-       
+
+        public string Unidade { get; set; }
+
+        public decimal ValorReferencia { get; set; }
+
+        public decimal ValorVenda { get; set; }
+
+        public float SaldoAtual { get; set; }
+
     }
 }
