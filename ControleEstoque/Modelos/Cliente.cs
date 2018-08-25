7e1023a38dca3c45c2b959ac329ad5f6fed174c8
moraismarcio <marcioc.morais@hotmail.com>
2018-08-25 20:40:25 -0300

    Implementando propriedades da classe (Modelo) Cliente

---
+++ ControleEstoque/Modelos/Cliente.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelos
+{
+    public class Cliente
+    {
+        // PROPRIEDADES
+        [Key]
+        public int ClienteId { get; set; }
+        public string NomeCliente { get; set; }
+
+        // RELACIONAMENTO
+    }
+}
