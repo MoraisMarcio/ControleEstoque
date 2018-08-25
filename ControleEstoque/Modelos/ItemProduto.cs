f9a9ef90faa585031aa68feacf0b92d1cb100458
moraismarcio <marcioc.morais@hotmail.com>
2018-08-25 20:09:26 -0300

    Criando propriedades da classe (Modelo) ItemProduto

---
+++ ControleEstoque/Modelos/ItemProduto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelos
+{
+    public class ItemProduto
+    {
+        // PROPRIEDADES
+
+        public decimal Preco { get; set; }
+
+        public float Quantidade { get; set; }
+
+        // RELACIONAMENTO
+
+    }
+}
