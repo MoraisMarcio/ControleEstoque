67bce7fb126ff9b45c841c17691485d54ff499e2
moraismarcio <marcioc.morais@hotmail.com>
2018-08-25 20:32:00 -0300

    Implementando propriedades da classe (Modelo) Fornecedor

---
+++ ControleEstoque/Modelos/Fornecedor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelos
+{
+    public class Fornecedor
+    {
+        // PROPRIEDADES
+        [Key]
+        public int FornecedorId { get; set; }
+        public string NomeFornecedor { get; set; }
+
+        // RELACIONAMENTO
+    }
+}
