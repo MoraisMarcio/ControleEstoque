5047b149771818ce95b2cca42b4ff79b03f968bc
moraismarcio <marcioc.morais@hotmail.com>
2018-08-30 11:46:07 -0300

    Alterações TelaClientes

--- ControleEstoque/ViewWPF/Views/Clientes/TelaClientes.xaml.cs
+++ ControleEstoque/ViewWPF/Views/Clientes/TelaClientes.xaml.cs
@@ -10,19 +10,23 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace ViewWPF
+namespace ViewWPF.Views.Clientes
 {
     /// <summary>
-    /// Interação lógica para MainWindow.xam
+    /// Lógica interna para TelaClientes.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class TelaClientes : Window
     {
-        public MainWindow()
+        public TelaClientes()
         {
             InitializeComponent();
+        }
+
+        private void btnCadastrarClientes_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
